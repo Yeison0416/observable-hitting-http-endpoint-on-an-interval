57eca8c748ccace78f6cde57368dbcff2b3be355
Sancy061 <Yeison.S.Sanchez.-ND@disney.com>
2025-11-19 15:56:56 -0500

    function rename

--- src/app/http-polling/http-polling.ts
+++ src/app/http-polling/http-polling.ts
@@ -29,7 +29,7 @@ export function HttpPolling(appRootNode: HTMLElement): HttpPolling {
         }
     }
 
-    function catsCheckBoxClickHandlers() {
+    function checkBoxClickHandlers() {
         if (!domElements) return;
 
         fromEvent(domElements?.catsCheckBox, 'click').subscribe(() => {
@@ -47,7 +47,7 @@ export function HttpPolling(appRootNode: HTMLElement): HttpPolling {
     async function initializeUI() {
         await renderInitialMarkUp();
         domElements = getDomElements();
-        catsCheckBoxClickHandlers();
+        checkBoxClickHandlers();
     }
 
     async function start() {
